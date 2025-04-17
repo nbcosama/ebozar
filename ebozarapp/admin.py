2a58cc62c9d980bb7d49fbe1f18c9f72b980f932
nbcosama <omd@nbc.edu.np>
2025-04-17 21:50:19 +0545

    newupdate

--- ebozarapp/admin.py
+++ ebozarapp/admin.py
@@ -1,12 +1,26 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Profile)
-admin.site.register(Product)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'store_name', 'user_type', 'verify')
+    search_fields = ('user__username', 'store_name', 'user_type')
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'product_category', 'get_store_name')
+    search_fields = ('product_name', 'product_category__category_name', 'user__store_name')
+
+    def get_store_name(self, obj):
+        return obj.user.store_name if obj.user else None
+    get_store_name.admin_order_field = 'user__store_name'
+    get_store_name.short_description = 'Store Name'
 admin.site.register(OTP)
 admin.site.register(Slug)
 admin.site.register(Secondary_images)
-admin.site.register(Searched_Query)
+@admin.register(Searched_Query)
+class SearchedQueryAdmin(admin.ModelAdmin):
+    list_display = ('query', 'date_time')
+    search_fields = ('query', )
 admin.site.register(ProductCategories)
 admin.site.register(buyerCart)
 admin.site.register(buyerOrder)
